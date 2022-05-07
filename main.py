45bfbff4d65f87fd0b4cdd7c208b6737745fe08c
BuildTools <unconfigured@null.spigotmc.org>
2022-05-07 23:43:14 +0900

    add study

--- main.py
+++ main.py
@@ -6,22 +6,39 @@ app = Flask(__name__)
 def running():
     return "Server is running"
 
-@app.route("/project", methods=['POST'])
+@app.route("/project",methods=['POST'])
 def project():
     req = request.get_json()
-    project = req["action"]["detailParams"]["project"]["value"]
-    res = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": project
+    try:
+        project = req["action"]["detailParams"]["project"]["value"]
+        response = "프로젝트: "+project
+        res = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": response
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
-    }
+    except:
+        study = req["action"]["detailParams"]["study"]["value"]
+        response = "스터디: "+study
+        res = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": response
+                        }
+                    }
+                ]
+            }
+        }
     return jsonify(res)
 
 if __name__ == "__main__":
